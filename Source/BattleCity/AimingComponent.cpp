14979b3d27d609ac2daf2ed6f467f0a880482484
Viktor Gomeniuk <viktor.gomeniuk@gmail.com>
2017-07-18 22:12:59 +0300

    Calculate Location of the Aim correctly and rotate Turret respectively.

--- Source/BattleCity/AimingComponent.cpp
+++ Source/BattleCity/AimingComponent.cpp
@@ -1,6 +1,8 @@
 // Viktor Gomeniuk : https://github.com/vgomeniuk
-
 #include "AimingComponent.h"
+#include "CoreMinimal.h"
+#include "Engine.h"
+#include "TurretComponent.h"
 
 
 // Sets default values for this component's properties
@@ -8,17 +10,39 @@ UAimingComponent::UAimingComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 }
 
 
-void UAimingComponent::AimAt(const FVector& AimLocation) {
-	UE_LOG(LogTemp, Warning, TEXT("%s aiming at: %s"), *(GetOwner()->GetName()), *AimLocation.ToString());
+void UAimingComponent::AimAt(const FVector& AimLocation, float LaunchSpeed) {
+	if (Turret == nullptr) { return; }
+
+	// Try to calculate Launch Velocity based on possible projectile trajectory
+	FVector LaunchVelocity;
+	FVector StartLocation = Turret->GetSocketLocation(FName("Projectile"));
+	bool bSolutionFound = UGameplayStatics::SuggestProjectileVelocity(
+		this, LaunchVelocity, StartLocation, AimLocation,
+		LaunchSpeed, false, 0, 0, ESuggestProjVelocityTraceOption::DoNotTrace
+	);
+
+	// If succeed
+	if (bSolutionFound) {
+		FVector AimDirection = LaunchVelocity.GetSafeNormal();
+		RotateTurret(AimDirection);
+	};
+	
 }
 
 
-void UAimingComponent::SetTurret(UStaticMeshComponent* Component) {
+void UAimingComponent::SetTurret(UTurretComponent* Component) {
 	Turret = Component;
+}
+
+
+void UAimingComponent::RotateTurret(FVector DesiredDirection) {
+	// Calculate rotation difference and rotate turret respectively
+	FRotator Delta = DesiredDirection.Rotation() - Turret->GetForwardVector().Rotation();
+	FMath::Abs(Delta.Yaw) > 180 ? Turret->Rotate(-Delta.Yaw) : Turret->Rotate(Delta.Yaw);
 }
--- Source/BattleCity/AimingComponent.h
+++ Source/BattleCity/AimingComponent.h
@@ -1,11 +1,10 @@
 // Viktor Gomeniuk : https://github.com/vgomeniuk
 
 #pragma once
-
-#include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "AimingComponent.generated.h"
 
+class UTurretComponent;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BATTLECITY_API UAimingComponent : public UActorComponent
@@ -13,12 +12,14 @@ class BATTLECITY_API UAimingComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UAimingComponent();
-	virtual void AimAt(const FVector& AimLocation);
-	virtual void SetTurret(UStaticMeshComponent* Component);
+	virtual void AimAt(const FVector& AimLocation, float LaunchSpeed);
+	virtual void SetTurret(UTurretComponent* Component);
+
+protected:
+	virtual void RotateTurret(FVector DesiredDirection);
 
 private:
-	UStaticMeshComponent* Turret;
+	UTurretComponent* Turret;
 	
 };
